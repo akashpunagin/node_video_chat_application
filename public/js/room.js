accad089673d5024f819554a04c30197197a972d
akashpunagin <akash.punagin@gmail.com>
2020-12-01 12:23:05 +0530

    accociated video elements with names as video id

--- public/js/room.js
+++ public/js/room.js
@@ -3,7 +3,6 @@ const socket = io();
 const videoGrid = $('#video-grid');
 const video = document.createElement('video');
 video.muted = true;
-let videoStreamCurrentUser;
 
 var peer = new Peer(undefined, {
   path: '/peerjs',
@@ -15,37 +14,28 @@ navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(function (stream) {
-  videoStreamCurrentUser = stream;
   addVideoStream(video, stream, USERNAME);
   socket.on('user-connected', function (userPeerId, username) {
     connectToNewUser(userPeerId, username, stream);
   });
 
-
   var peerUsername;
-  peer.on('connection', function(conn) {
-    console.log("PEER CONNECTION");
-    conn.on('data', function(data) {
-      console.log("PEER CONNECTION DATA:",data);
-      peerUsername = data;
-      console.log("SET PEER USERNAME");
+  peer.on('connection', function (conn) {
+    conn.on('data', function (username) {
+      peerUsername = username;
     });
   });
-
-
   peer.on('call', function (call) {
     console.log("PEER CALL", call);
     call.answer(stream);
     const video = document.createElement('video');
     call.on('stream', function (userVideoStream) {
-      console.log("PEER USER NAME TO STREAM: ", peerUsername);
-      addVideoStream(video, userVideoStream, peerUsername); // TODO: pass username
+      addVideoStream(video, userVideoStream, peerUsername);
     });
   });
 
 }).catch(function (err) {
   alert("Couldn't connect to your device's media");
-  alert(err); // TODO: delete
 });
 
 peer.on('open', function (id) {
@@ -57,21 +47,15 @@ peer.on('open', function (id) {
   });
 });
 
-
 const connectToNewUser = function (userPeerId, username, stream) {
-
   var conn = peer.connect(userPeerId);
   conn.on('open', function(){
-    console.log("SENDING DATA: ", USERNAME);
     conn.send(USERNAME);
-  });
-
-  const call = peer.call(userPeerId, stream);
-
-  const video = document.createElement('video');
-  call.on('stream', function (userVideoStream) {
-    console.log("CONNECT TO NEW USER", username);
-    addVideoStream(video, userVideoStream, username);
+    const call = peer.call(userPeerId, stream);
+    const video = document.createElement('video');
+    call.on('stream', function (userVideoStream) {
+      addVideoStream(video, userVideoStream, username);
+    });
   });
 };
 
